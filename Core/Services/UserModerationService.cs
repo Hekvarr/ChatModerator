41080362c14180993829848065db5fad993d6f69
Hekvarr <178476581+Hekvarr@users.noreply.github.com>
2025-06-08 19:19:51 -0300

    feature: add service UserModerationService

---
+++ Core/Services/UserModerationService.cs
@@ -0,0 +1,13 @@
+﻿using Core.Data.Entities;
+
+namespace Core.Services;
+
+public class UserModerationService(IContentModerationService contentModerationService, IUserService userService)
+    : IUserModerationService
+{
+    public async Task ProcessMessage(Message message)
+    {
+        var messageIsOffensive = await contentModerationService.IsOffensive(message.Content);
+        if (messageIsOffensive) await userService.BanTemporarily(message.UserId, DateTime.Now.AddHours(1));
+    }
+}
