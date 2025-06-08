f54a5c378aa557999115c95e188226706b647f8b
Hekvarr <178476581+Hekvarr@users.noreply.github.com>
2025-06-08 18:07:57 -0300

    feature: add event MessageCreatedEvent

---
+++ Core/Data/Events/MessageCreatedEvent.cs
@@ -0,0 +1,6 @@
+﻿using Core.Data.Entities;
+
+namespace Core.Data.Events;
+
+public record MessageCreatedEvent(Guid Id, Message Payload, long Timestamp)
+    : Event<Message>(nameof(MessageCreatedEvent), Id, Payload, Timestamp);
