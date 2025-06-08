92d85e8721bac0ac3443d21d17cd9fcbf935befc
Hekvarr <178476581+Hekvarr@users.noreply.github.com>
2025-06-08 19:18:14 -0300

    feature: add service UserService

---
+++ Core/Services/UserService.cs
@@ -0,0 +1,45 @@
+﻿using Core.Data.Repositories;
+using Core.Exceptions;
+
+namespace Core.Services;
+
+public class UserService(IUserRepository repository) : IUserService
+{
+    public async Task BanForever(int userId)
+    {
+        var user = await repository.GetById(userId);
+        if (user == null) throw new UserNotFoundException("User with the specified ID was not found.");
+        if (user.IsBannedForever)
+            throw new UserAlreadyBannedException("User is already banned and cannot be banned again.");
+        user.IsBannedForever = true;
+        await repository.Update(user);
+    }
+
+    public async Task BanTemporarily(int userId, DateTime bannedUntil)
+    {
+        if (bannedUntil <= DateTime.UtcNow)
+            throw new InvalidBanPeriodException("The ban end date must be in the future.");
+        var user = await repository.GetById(userId);
+        if (user == null) throw new UserNotFoundException("User with the specified ID was not found.");
+        if (user.IsBannedForever)
+            throw new UserAlreadyBannedException("User is already banned and cannot be banned again.");
+        if (user.IsTemporarilyBanned && bannedUntil < user.BannedUntil)
+            throw new InvalidBanPeriodException("The new ban period is shorter than the current one.");
+        user.IsTemporarilyBanned = true;
+        user.BannedUntil = bannedUntil;
+        await repository.Update(user);
+    }
+
+    public async Task LiftTemporarilyBan(int userId)
+    {
+        var user = await repository.GetById(userId);
+        if (user == null) throw new UserNotFoundException("User with the specified ID was not found.");
+        if (user.IsBannedForever)
+            throw new UserPermanentlyBannedException("Cannot lift temporary ban: user is permanently banned.");
+        if (!user.IsTemporarilyBanned)
+            throw new UserNotTemporarilyBannedException("User is not temporarily banned.");
+        user.IsTemporarilyBanned = false;
+        user.BannedUntil = null;
+        await repository.Update(user);
+    }
+}
