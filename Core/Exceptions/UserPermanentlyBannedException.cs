30e3ab943d30e41dc3f3f0d0e50c1cb2e72917c6
Hekvarr <178476581+Hekvarr@users.noreply.github.com>
2025-06-08 19:14:51 -0300

    feature: add exception UserPermanentlyBannedException

---
+++ Core/Exceptions/UserPermanentlyBannedException.cs
@@ -0,0 +1,16 @@
+﻿namespace Core.Exceptions;
+
+public class UserPermanentlyBannedException : Exception
+{
+    public UserPermanentlyBannedException()
+    {
+    }
+
+    public UserPermanentlyBannedException(string? message) : base(message)
+    {
+    }
+
+    public UserPermanentlyBannedException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
