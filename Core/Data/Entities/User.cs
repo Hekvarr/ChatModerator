0c1933e130a263c23ca63788a154f50669b97e4d
Hekvarr <178476581+Hekvarr@users.noreply.github.com>
2025-06-08 17:52:41 -0300

    feature: add entity User

---
+++ Core/Data/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Data.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool IsBanedForever { get; set; }
+    public bool IsTemporarilyBaned { get; set; }
+    public DateTime? BanedUntil { get; set; }
+}
