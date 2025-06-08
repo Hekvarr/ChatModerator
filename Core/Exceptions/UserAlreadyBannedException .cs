d8828a78f8459763a22c06608781404348580914
Hekvarr <178476581+Hekvarr@users.noreply.github.com>
2025-06-08 19:14:11 -0300

    feature: add exception UserAlreadyBannedException

---
+++ Core/Exceptions/UserAlreadyBannedException .cs
@@ -0,0 +1,16 @@
+﻿namespace Core.Exceptions;
+
+public class UserAlreadyBannedException : Exception
+{
+    public UserAlreadyBannedException()
+    {
+    }
+
+    public UserAlreadyBannedException(string? message) : base(message)
+    {
+    }
+
+    public UserAlreadyBannedException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
