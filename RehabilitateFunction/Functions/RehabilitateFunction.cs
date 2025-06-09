bde10f5bb8ab0e7440057264c0a44a04f20053cd
Hekvarr <178476581+Hekvarr@users.noreply.github.com>
2025-06-08 22:57:38 -0300

    feat: add function `RehabilitateFunction`

---
+++ RehabilitateFunction/Functions/RehabilitateFunction.cs
@@ -0,0 +1,22 @@
+﻿using Core.Services;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace RehabilitateFunction.Functions;
+
+public class RehabilitateFunction(IUserService service, ILogger<RehabilitateFunction> logger)
+{
+    [Function("RehabilitateFunction")]
+    public async Task Run([TimerTrigger("0 * * * *")] TimerInfo timer)
+    {
+        try
+        {
+            await service.LiftTemporaryBans();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
