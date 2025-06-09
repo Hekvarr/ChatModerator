78961211f0b37ce76c7e9ec26f42768b29e5d159
Hekvarr <178476581+Hekvarr@users.noreply.github.com>
2025-06-08 22:58:00 -0300

    feat: edit `Program.cs`

--- RehabilitateFunction/Program.cs
+++ RehabilitateFunction/Program.cs
@@ -1,5 +1,10 @@
+using Core.Data;
+using Core.Data.Repositories;
+using Core.Services;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -10,5 +15,10 @@ builder.ConfigureFunctionsWebApplication();
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 builder.Build().Run();
