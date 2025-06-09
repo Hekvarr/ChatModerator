77229e0002025837bda79887407355f14beb5e0d
Hekvarr <178476581+Hekvarr@users.noreply.github.com>
2025-06-08 22:26:55 -0300

    feat: edit `Program.cs`

--- BanUserFunction/Program.cs
+++ BanUserFunction/Program.cs
@@ -1,11 +1,21 @@
+using Core.Data;
+using Core.Data.Repositories;
+using Core.Services;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
